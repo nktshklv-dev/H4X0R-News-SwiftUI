1b5d8e89b35e699625667a921ba7277c81a7eda6
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-09-22 20:20:25 +0300

    Initial Commit

---
+++ H4X0R News-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  H4X0R News-SwiftUI
+//
+//  Created by Nikita  on 9/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
